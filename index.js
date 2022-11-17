3d788c3580b25048799438e667dd87e184b3c788
Xenomii <voltzxp@gmail.com>
2022-11-17 14:50:55 +0800

    Pract test

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-var express = require('express')
+var express = require('express');
+var session = require('express-session');
+var path = require('path');
+var owasp = require('owasp-password-strength-test');
 
 var app = express();
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+owasp.config({
+    allowPassphrases: false,
+    maxLength: 128,
+    minLength: 8,
+})
 
 app.get('/', function(req, res) {
-    res.send('Hello World!');
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.post('/auth', function(req, res) {
+    let password = req.body.password;
+    console.log(password)
+    var result = owasp.test(password);
+    console.log(result);
+    if(result.errors.length == 0) {
+        res.send("Password is valid");
+    } else {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    }
 });
 
 app.listen(process.env.PORT || 3000);
